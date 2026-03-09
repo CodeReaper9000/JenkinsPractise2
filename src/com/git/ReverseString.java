560a917787b090d703c2e8f5be315fd4a0e6909b
CodeReaper9000 <swastik33roy@gmail.com>
2026-03-09 14:20:49 +0530

    first commit

---
+++ src/com/git/ReverseString.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "abcdefgh";
+		char c[] = s.toCharArray();
+		int l=0, r=s.length();
+		while(l<=r) {
+			char temp = c[l];
+			c[l] = c[r];
+			c[r] = temp;
+			l++;r--;
+		}
+		System.out.println(new String(c));
+	}
+
+}
